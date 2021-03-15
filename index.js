9b44da0e7ce521250c116db8c41cc3f9c1672268
Sherry <Shahryartariq57@gmail.com>
2021-03-15 15:55:15 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Working Fine 😊');
+
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+ 
+
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    
+})
